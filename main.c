55685603630a945626986e35e5429b69d802b41c
Andre Filipe Ferreira Miranda <andrferr@c2r5p8.hive.fi>
2023-01-23 12:25:26 +0200

    main edited

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 11:33:12 by andrferr          #+#    #+#             */
-/*   Updated: 2023/01/23 11:33:20 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/01/23 12:24:14 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-THis is a shit main!!!!
+//THis is a shit main!!!!
