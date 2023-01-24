ed6b6c0085a4ce676717d98e54edefc77b11ba08
Joao <nascimento.joaogb@gmail.com>
2023-01-24 11:01:39 +0200

    first push from 24.01 - Joao

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: jonascim <jonascim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 16:00:09 by andrferr          #+#    #+#             */
-/*   Updated: 2023/01/23 16:00:39 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/01/24 10:58:58 by jonascim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <stdio.h>
 
 #endif
--- main.c
+++ main.c
@@ -6,22 +6,45 @@
 /*   By: jonascim <jonascim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 11:33:12 by andrferr          #+#    #+#             */
-/*   Updated: 2023/01/23 13:54:37 by jonascim         ###   ########.fr       */
+/*   Updated: 2023/01/24 10:56:19 by jonascim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./includes/minishell.h"
 
-//THis is a shit main!!!!
-//HEEEEEEy
-
-#include <stdio.h>
-
-int main(void)
+void	exit_message(char *error)
 {
-	printf("Hello World\n");
-	printf("This print comes from the test branch\n");
-	printf("wharever");
-	return (0);
+	ft_putstr_fd(error, 2);
+	exit(1);
 }
 
+int	fork_create(void)
+{
+	int	pid;
 
+	pid = fork();
+	if (pid < 0)
+		exit_message("Fork error.\n");
+	else
+		return (pid);
+}
+
+int	main(int argc, char **argv)
+{
+	char	**array;
+	int	fd;
+
+	if (argc < 2)
+		error_message("invalid input.\n");
+	else
+		array = get_input(argc, argv);
+	while ((fd = open("console", O_RDWR)) >= 0)
+	{
+		if (fd >=3)
+		{
+			close(fd);
+			break ;
+		}
+	}
+	return (0);
+}
