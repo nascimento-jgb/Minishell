6e740539ebc1da1d635b801ab14a576bfa572133
Andre Filipe Ferreira Miranda <andrferr@c2r6p12.hive.fi>
2023-02-13 13:10:07 +0200

    added some extra cleaning in the clean function

--- includes/structs.h
+++ includes/structs.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   structs.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jonascim <jonascim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 08:44:56 by andrferr          #+#    #+#             */
-/*   Updated: 2023/02/11 13:19:38 by jonascim         ###   ########.fr       */
+/*   Updated: 2023/02/13 13:06:38 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ typedef struct s_minishell
 	char	*paths;
 	char	**cmd_paths;
 	char	**cmd_args;
+	char	*last_cmd:
 }	t_minishell;
 
 typedef struct	s_command
