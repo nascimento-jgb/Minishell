a4883ba6b5d4906081e7b7127665fba294fc9708
Joao <nascimento.joaogb@gmail.com>
2023-02-13 17:24:28 +0200

    builtins check

---
+++ srcs/parser/check_builtins.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_builtins.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jonascim <jonascim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 10:27:48 by jonascim          #+#    #+#             */
+/*   Updated: 2023/02/13 17:24:13 by jonascim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	builtins_check(t_minishell *minishell)
+{
+	if (!ft_strncmp(minishell->capturedLine, "cd ", 3))
+		ms_cd(minishell->capturedLine + 3);
+	else if (!ft_strncmp(minishell->capturedLine, "echo ", 5))
+	{
+		if (minishell->capturedLine[5] == '-' && minishell->capturedLine[6] == 'n')
+			ms_echo(minishell->capturedLine + 7, 1);
+		else
+			ms_echo(minishell->capturedLine + 5, 0);
+	}
+	else if (!ft_strncmp(minishell->capturedLine, "exit ", 5))
+		ms_exit(0);
+	else if (!ft_strncmp(minishell->capturedLine, "env ", 4))
+		ms_env(minishell->env->vars_list);
+	else
+	{
+		if (fork_create() == 0)
+			run_command(parse_command(minishell->capturedLine), minishell);
+		wait(NULL);
+	}
+}
