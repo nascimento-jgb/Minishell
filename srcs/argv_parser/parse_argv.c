f21f58f0bf993a5cc826e53ea0e7db87ac4d1d83
Andre Filipe Ferreira Miranda <andrferr@c2r5p12.hive.fi>
2023-02-17 17:49:11 +0200

    argv pasring loop started

--- srcs/argv_parser/parse_argv.c
+++ srcs/argv_parser/parse_argv.c
@@ -6,7 +6,7 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 11:41:32 by andrferr          #+#    #+#             */
-/*   Updated: 2023/02/17 16:56:44 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/02/17 17:48:52 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,21 @@ int	count_argc(char *str)
 	}
 	ft_printf("argc: %d\n", count);
 	return (count);
+}
+
+int	count_letters(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (ft_isspace(str[i]) && !quotes_stage(str, i))
+			break ;
+		i++;
+	}
+	ft_printf("letters: %d\n", i);
+	return (i);
 }
 
 int argv_parser(t_minishell *minishell)
@@ -52,19 +67,26 @@ int argv_parser(t_minishell *minishell)
 		j = 0;
 		while (ft_isspace(*str))
 			str++;
-		k = 0;
-		ft_printf("str: %s\n", str);
-		while (!ft_isspace(*str) && !quotes_stage(str, k))
+		argv[i] = ft_calloc(count_letters(str), sizeof(char));
+		if (!argv[i])
 		{
-			if (*(str + k) == 34)
-				k++;
-			else
+			free_char_arr(argv);
+			return (1);
+		}
+		k = 0;
+		ft_printf("str: %s stage: %d\n", str, quotes_stage(str, k));
+
+		//CHECK FROM HERE! REMEMBER SPACE IS ONLY DELIMETER IF QUOTES IS CONSIDERED CLOSED!!!!!!!
+		while (!(ft_isspace(*(str + 1)) && quotes_stage(str, k)) && *str)
+		{
+			if (*(str + k) != 34)
 			{
-				write(1, "#", 1);
 				argv[i][j++] = *(str + k);
 			}
 			k++;
+			str++;
 		}
+		str++;
 		i++;
 	}
 	argv[i] = NULL;
